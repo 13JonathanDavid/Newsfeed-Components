80b488fc978076ab8fca1259618fb7610f37ad81
13JonathanDavid <48808640+13JonathanDavid@users.noreply.github.com>
2019-04-10 18:55:54 -0400

    completed goals and stretch

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,27 @@
 
 const toggleMenu = () => {
-  // Toggle the "menu--open" class on your menu refence. 
+  // Toggle the "menu--open" class on your menu refence.
+  menu.classList.toggle('menu--open');
 }
 
 // Start Here: Create a reference to the ".menu" class
-const menu;
+const menu = document.getElementsByClassName('menu')[0];
+
+class menuitem
+{
+  constructor(element)
+  {
+    this.element = element;
+    this.element.addEventListener("click", x=> this.clicked())
+  }
+  clicked()
+  {
+    console.log("clicked");
+  }
+}
 // create a reference to the ".menu-button" class
-const menuButton;
+const menuButton = document.getElementsByClassName('menu-button')[0];
 // Using your menuButton reference, add a click handler that calls toggleMenu
+menuButton.addEventListener("click", function(){
+  toggleMenu();
+});
